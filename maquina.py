0ef1ce4845967a3b61eeb920f67f34aefe70bb23
At0micAnt <pedrodafonsecaschwertner@gmail.com>
2025-06-05 08:37:45 -0300

    Correction of operation exits

--- maquina.py
+++ maquina.py
@@ -115,57 +115,65 @@ def modo_admin(): #permite que o amd gerencie os produtos
             for linha in matriz:
                 id_prod, preco, qtd = linha
                 print(f"[{id_prod}] {nome_produtos[id_prod]} - R${preco:.2f} | Estoque: {qtd}")
-            
-            try:
-                id_editar = int(input("Digite o ID do produto que deseja editar: "))
-            except ValueError:
-                print("ID inválido. Use apenas números.")
-                continue
 
-            for i, (pid) in enumerate(matriz):
-                if pid == id_editar:
-                    while True:
-                        print(f"\n--- Editar produto: {nome_produtos[pid]} ---")
-                        print("[1] Editar nome")
-                        print("[2] Editar preço")
-                        print("[3] Editar estoque")
-                        print("[0] Voltar")
-                        try:
-                            opcao_edicao = int(input("Escolha a opção: "))
-                        except ValueError:
-                            print("Opção inválida. Use apenas números.")
-                            continue
+            while True:
+                try:
+                    id_editar = int(input("Digite o ID do produto que deseja editar (ou -1 para voltar): "))
+                except ValueError:
+                    print("ID inválido. Use apenas números.")
+                    continue
 
-                        if opcao_edicao == 1:
-                            novo_nome = input("Novo nome do produto: ").strip()
-                            if novo_nome:
-                                nome_produtos[pid] = novo_nome
-                                print("Nome atualizado!")
-                            else:
-                                print("Nome não alterado.")
-                        elif opcao_edicao == 2:
-                            try:
-                                novo_preco = float(input("Novo preço do produto: "))
-                                matriz[i][1] = novo_preco
-                                print("Preço atualizado!")
-                            except ValueError:
-                                print("Preço inválido.")
-                        elif opcao_edicao == 3:
-                            try:
-                                novo_estoque = int(input("Novo estoque do produto: "))
-                                matriz[i][2] = novo_estoque
-                                print("Estoque atualizado!")
-                            except ValueError:
-                                print("Estoque inválido.")
-                        elif opcao_edicao == 0:
-                            print("Voltando ao menu admin...")
-                            break
-                        else:
-                            print("Opção inválida.")
+                if id_editar == -1:
                     break
-            else:
-                print("Produto com esse ID não encontrado.")
-                
+
+                encontrado = False
+                for i, (pid, _, _) in enumerate(matriz):
+                    if pid == id_editar:
+                        encontrado = True
+                        while True:
+                            print(f"\n--- Editar produto: {nome_produtos[pid]} ---")
+                            print("[1] Editar nome")
+                            print("[2] Editar preço")
+                            print("[3] Editar estoque")
+                            print("[0] Voltar")
+                            try:
+                                opcao_edicao = int(input("Escolha a opção: "))
+                            except ValueError:
+                                print("Opção inválida. Use apenas números.")
+                                continue
+
+                            if opcao_edicao == 1:
+                                novo_nome = input("Novo nome do produto: ").strip()
+                                if novo_nome:
+                                    nome_produtos[pid] = novo_nome
+                                    print("Nome atualizado!")
+                                else:
+                                    print("Nome não alterado.")
+                            elif opcao_edicao == 2:
+                                print(f"Preço atual: {matriz[i][1]}")
+                                try:
+                                    novo_preco = float(input("Novo preço do produto: "))
+                                    matriz[i][1] = novo_preco
+                                    print("Preço atualizado!")
+                                except ValueError:
+                                    print("Preço inválido.")
+                            elif opcao_edicao == 3:
+                                print(f"Estoque atual: {matriz[i][2]}")
+                                try:
+                                    novo_estoque = int(input("Novo estoque do produto: "))
+                                    matriz[i][2] = novo_estoque
+                                    print("Estoque atualizado!")
+                                except ValueError:
+                                    print("Estoque inválido.")
+                            elif opcao_edicao == 0:
+                                print("Voltando ao menu de edição de produtos...\n")
+                                break
+                            else:
+                                print("Opção inválida.")
+                        break
+                if not encontrado:
+                    print("Produto com esse ID não encontrado. Tente novamente.")
+
         elif operacao == 3:
             for linha in matriz:
                     id_produto = linha[0]
@@ -203,50 +211,87 @@ while True:
                             [2] Administrador
                             [3] Sair
                             Sua opção: '''))
+        
         if modo == 2:
             modo_admin()
+
         elif modo == 1:
-            for linha in matriz:
-                id_produto = linha[0]
-                nome = nome_produtos.get(id_produto, "Nome desconhecido")
-                preco = linha[1]
-                estoque = linha[2]
-                print(f"[{id_produto}] {nome} - R${preco:.2f} |")
-            while True:
-                id_digitado = int(input('ID desejado: '))
-                produto_encontrado = None
+            modo_visitante_ativo = True
+            while modo_visitante_ativo:
+                print("\n--- Lista de Produtos ---")
                 for linha in matriz:
-                    if linha[0] == id_digitado:
-                        produto_encontrado = linha
+                    id_produto = linha[0]
+                    nome = nome_produtos.get(id_produto, "Nome desconhecido")
+                    preco = linha[1]
+                    print(f"[{id_produto}] {nome} - R${preco:.2f}")
+
+                while True:
+                    try:
+                        id_digitado = int(input("ID desejado: "))
+                    except ValueError:
+                        print("Digite um número válido.")
+                        continue
+
+                    produto_encontrado = None
+                    for linha in matriz:
+                        if linha[0] == id_digitado:
+                            produto_encontrado = linha
+                            break
+                    if produto_encontrado:
                         break
-                if produto_encontrado:
-                    break
-                else:
-                    print("Selecione a bebida disponível:")
-            nome = nome_produtos[produto_encontrado[0]]
-            valor = produto_encontrado[1]
-            estoque = produto_encontrado[2]
-
-            print(f"Produto: {nome} | Preço: R${valor} | Estoque: {estoque}")
-
-            while True:
-                valor_inserido = float(input('Insira o valor inserido na máquina: '))
-                if valor_inserido >= valor:
-                    print(f"Você vai pagar R${valor}")
-                    troco = calcularTroco(valor, valor_inserido)
-                    if troco is None:
-                        print("Não há troco suficiente na máquina. Compra cancelada.")
                     else:
-                        print("Troco: ")
-                        for moeda, qtd in troco.items():
-                            print(f"R${moeda:.2f} x {qtd}")
-                        produto_encontrado[2] -= 1  # atualiza estoque
-                    break
-                else:
-                    print("Valor insuficiente. Insira um valor maior.")
+                        print("Produto não encontrado. Selecione um ID válido.")
+
+                nome = nome_produtos[produto_encontrado[0]]
+                valor = produto_encontrado[1]
+                estoque = produto_encontrado[2]
+
+                print(f"Produto: {nome} | Preço: R${valor:.2f} | Estoque: {estoque}")
+
+                while True:
+                    try:
+                        valor_inserido = float(input('Insira o valor inserido na máquina: '))
+                    except ValueError:
+                        print("Valor inválido.")
+                        continue
+
+                    if valor_inserido >= valor:
+                        print(f"Você vai pagar R${valor:.2f}")
+                        troco = calcularTroco(valor, valor_inserido)
+                        if troco is None:
+                            print("Não há troco suficiente na máquina. Compra cancelada.")
+                        else:
+                            print("Troco: ")
+                            for moeda, qtd in troco.items():
+                                print(f"R${moeda:.2f} x {qtd}")
+                            produto_encontrado[2] -= 1  # Atualiza o estoque
+
+                        while True:
+                            try:
+                                continuar = int(input("""Deseja continuar compra?
+                                                        [1] Continuar
+                                                        [2] Sair
+                                                        R: """))
+                                if continuar == 1:
+                                    break  # Volta ao início do modo visitante
+                                elif continuar == 2:
+                                    print("Saindo do modo de compra...\n")
+                                    modo_visitante_ativo = False
+                                    break
+                                else:
+                                    print("Opção inválida.")
+                            except ValueError:
+                                print("Digite apenas 1 ou 2.")
+                        break  # Sai do loop de pagamento
+                    else:
+                        print("Valor insuficiente. Insira um valor maior.")
+
         elif modo == 3:
+            print("Encerrando o programa.")
             break
+
         else:
             print("Opção inválida.")
+
     except Exception as e:
         print(f"Ocorreu um erro: {e}")
